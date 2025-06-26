7aa48931b32427ccbb8cfe2e0da0bad6a86ebfab
daniel-ibok <etimdaniel08@gmail.com>
2025-06-26 18:30:53 +0100

    configured vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/crowdfunding-dapp",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
